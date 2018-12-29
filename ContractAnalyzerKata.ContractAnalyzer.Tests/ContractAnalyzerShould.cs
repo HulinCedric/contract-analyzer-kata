798053c11751e36d87197f03700d3bce85e13604
Cédric Hulin <hulin.cedric@gmail.com>
2018-12-29 19:44:16 +0100

    refactor: Renaming test (DDD principles)

--- ContractAnalyzerKata.ContractAnalyzer.Tests/ContractAnalyzerShould.cs
+++ ContractAnalyzerKata.ContractAnalyzer.Tests/ContractAnalyzerShould.cs
@@ -2,7 +2,7 @@ using Xunit;
 
 namespace ContractAnalyzerKata.ContractAnalyzer.Tests
 {
-    public class ContractAnalyserTest
+    public class ContractAnalyzerShould
     {
         [Fact]
         public void DummyTest()
