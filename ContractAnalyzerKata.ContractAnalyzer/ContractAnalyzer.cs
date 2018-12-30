915b9e27a4f5bd494ccbca9ee72e893e7bfb8b0b
Cédric Hulin <hulin.cedric@gmail.com>
2018-12-30 11:23:09 +0100

    refactor: Replace fraud rule and under age rule member by rules

--- ContractAnalyzerKata.ContractAnalyzer/ContractAnalyzer.cs
+++ ContractAnalyzerKata.ContractAnalyzer/ContractAnalyzer.cs
@@ -8,36 +8,26 @@ namespace ContractAnalyzerKata.ContractAnalyzer
     public class ContractAnalyzer
     {
         private readonly IList<Violation> _violations = new List<Violation>();
-        private readonly Rule _fraudRule;
-        private readonly Rule _underAgeRule = new UnderAgeRule();
+        private readonly IEnumerable<Rule> _rules;
 
         public ContractAnalyzer() : this(new FraudDetectorAdapter()) { }
 
-        public ContractAnalyzer(IFraudDetector fraudDetector) => _fraudRule = new FraudRule(fraudDetector);
+        public ContractAnalyzer(IFraudDetector fraudDetector) => _rules = new List<Rule> {
+            new UnderAgeRule(),
+            new FraudRule(fraudDetector)
+        };
 
         public IEnumerable<Violation> Violations => _violations;
 
         public void Analyze(Contract contract)
         {
-            UnderAgeRuleCheck(contract);
-            FraudRuleCheck(contract);
-        }
-
-        private void FraudRuleCheck(Contract contract)
-        {
-            _fraudRule.Check(contract);
-            if (_fraudRule.HasViolation)
+            foreach (var rule in _rules)
             {
-                _violations.Add(_fraudRule.Violation);
-            }
-        }
-
-        private void UnderAgeRuleCheck(Contract contract)
-        {
-            _underAgeRule.Check(contract);
-            if (_underAgeRule.HasViolation)
-            {
-                _violations.Add(_underAgeRule.Violation);
+                rule.Check(contract);
+                if (rule.HasViolation)
+                {
+                    _violations.Add(rule.Violation);
+                }
             }
         }
     }
