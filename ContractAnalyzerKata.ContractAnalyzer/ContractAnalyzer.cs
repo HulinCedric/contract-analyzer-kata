d9d4a5e81d0ed7d27b96185afd4feb84ad98f7f5
Cédric Hulin <hulin.cedric@gmail.com>
2018-12-30 12:16:27 +0100

    refactor: Move violations logic into rule

--- ContractAnalyzerKata.ContractAnalyzer/ContractAnalyzer.cs
+++ ContractAnalyzerKata.ContractAnalyzer/ContractAnalyzer.cs
@@ -7,24 +7,19 @@ namespace ContractAnalyzerKata.ContractAnalyzer
 {
     public class ContractAnalyzer
     {
-        private readonly IList<Violation> _violations = new List<Violation>();
         private readonly IEnumerable<Rule> _rules;
 
         public ContractAnalyzer() : this(new FraudDetectorAdapter()) { }
 
         public ContractAnalyzer(IFraudDetector fraudDetector) => _rules = RuleFactory.GetRules(fraudDetector);
 
-        public IEnumerable<Violation> Violations => _violations;
+        public IEnumerable<Violation> Violations => _rules.Where(rule => rule.HasViolation).Select(rule => rule.Violation);
 
         public void Analyze(Contract contract)
         {
             foreach (var rule in _rules)
             {
                 rule.Check(contract);
-                if (rule.HasViolation)
-                {
-                    _violations.Add(rule.Violation);
-                }
             }
         }
     }
